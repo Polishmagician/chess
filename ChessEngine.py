5b6e99a2a07a488a2bcd9f7c6e50e9f629fa27bb
Polishmagician <fcjory@gmail.com>
2024-12-02 21:41:01 +0100

    wow

--- ChessEngine.py
+++ ChessEngine.py
@@ -14,3 +14,35 @@ class GameState():
             ["wR","wN","wB","wQ","wK","wB","wN","wR"]]
         self.whiteToMove = True
         self.movelog = []
+class piece():
+    def __init__(self,color,position):
+        self.color = color
+        self.moved = False
+        self.position = position
+class pawn(piece):
+    def __init__(self, color, position):
+        super().__init__(color, position)
+        pawn.piece = "p"
+        pawn.firstrank = True
+class rook(piece):
+    def __init__(self, color, position):
+        super().__init__(color, position)
+        rook.piece = "R"
+        rook.castle = True
+class knight(piece):
+    def __init__(self, color, position):
+        super().__init__(color, position)
+        knight.piece = "K"
+class bishop(piece):
+    def __init__(self, color, position):
+        super().__init__(color, position)
+        bishop.piece = "B"
+class queen(piece):
+    def __init__(self, color, position):
+        super().__init__(color, position)
+        queen.piece = "Q"
+class king(piece):
+    def __init__(self, color, position):
+        super().__init__(color, position)
+        king.piece = "K"
+        king.castle = True
--- chessMain.py
+++ chessMain.py
@@ -1,5 +1,5 @@
 import pygame as p
-import ChessEngine
+from ChessEngine import *
 from movement import *
 
 width = height = 400
@@ -7,6 +7,7 @@ dimension = 8
 sq_size = height/dimension
 max_fps = 15 #Animations later on
 images = {}
+start_pieces = {}
 whitepieces = ["wR","wN","wB","wQ","wK","wB","wN","wR","wp"]
 blackpieces = ["bR","bN","bB","bQ","bK","bB","bN","bR","bp"]
 #Initialize global dict of images. This will be calles once in the main
@@ -16,6 +17,30 @@ def loadImages():
     for piece in pieces:
         images[piece] = p.transform.scale(p.image.load(f"images/{piece}.svg"),(sq_size,sq_size))
     #Note we can acces an image by saying 'images['wp']
+def start_position():
+    white_pawns = ["wp1","wp2","wp3","wp4","wp5","wp6","wp7","wp8"]
+    for index,p in enumerate(white_pawns):
+        start_pieces[p] = pawn("w",[6,index])
+    black_pawns = ["bp1","bp2","bp3","bp4","bp5","bp6","bp7","bp8"]
+    for index,p in enumerate(black_pawns):
+        start_pieces[p] = pawn("w",[1,index])
+    start_pieces["wR1"] = rook("w",[7,0])
+    start_pieces["bR1"] = rook("b",[0,0])
+    start_pieces["wR2"] = rook("w",[7,7])
+    start_pieces["bR2"] = rook("b",[0,7])
+    start_pieces["wB1"] = rook("w", [7, 2])
+    start_pieces["bB1"] = rook("b", [0, 2])
+    start_pieces["wB2"] = rook("w", [7, 5])
+    start_pieces["bB2"] = rook("b", [0, 5])
+    start_pieces["wN1"] = rook("w", [7, 1])
+    start_pieces["bN1"] = rook("b", [0, 1])
+    start_pieces["wN2"] = rook("w", [7, 6])
+    start_pieces["bN2"] = rook("b", [0, 6])
+    start_pieces["wQ1"] = rook("w", [7, 3])
+    start_pieces["bQ1"] = rook("b", [0, 3])
+    start_pieces["wK1"] = rook("w", [7, 4])
+    start_pieces["bK1"] = rook("b", [0, 4])
+
 
 #The main driver. This will handle user input and updating graphics
 def main():
@@ -23,8 +48,9 @@ def main():
     screen = p.display.set_mode((width,height))
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
-    gs = ChessEngine.GameState()
+    gs = GameState()
     loadImages() #only once, before the while loop
+    start_position()
     running = True
     while running:
         for e in p.event.get():
@@ -34,18 +60,26 @@ def main():
             if e.type == p.MOUSEBUTTONDOWN: #Piece pressed
                 current_pos = p.mouse.get_pos()
                 current_coord = [int(current_pos[1]//sq_size), int(current_pos[0]//sq_size)]
-                clicked_piece = gs.board[current_coord[0]][current_coord[1]]
+                for name,item in start_pieces.items():
+                    if item.position == current_coord:
+                        clicked_piece = name
+                        print(clicked_piece)
+                        print(start_pieces[clicked_piece].position)
+                    # else:
+                    #     clicked_piece = ""
             if e.type == p.MOUSEBUTTONUP:
-                if clicked_piece != "":
-                    new_pos = p.mouse.get_pos()
-                    new_coord = [int(new_pos[1]//sq_size), int(new_pos[0]//sq_size)]
-                    possible_coord = movement_piece(gs,current_coord,clicked_piece)
-                    if new_coord in possible_coord:
-                        print("ok")
-                    else:
-                        print("false")
-                else:
-                    continue
+                # if clicked_piece != "":
+                new_pos = p.mouse.get_pos()
+                new_coord = [int(new_pos[1]//sq_size), int(new_pos[0]//sq_size)]
+                start_pieces[clicked_piece].position = new_coord
+                print(start_pieces[clicked_piece].position)
+                    # possible_coord = movement_piece(gs,current_coord,clicked_piece)
+                    # if new_coord in possible_coord:
+                    #     print("ok")
+                    # else:
+                    #     print("false")
+                # else:
+                #     continue
         clock.tick(max_fps)
         p.display.flip()
 
@@ -70,8 +104,6 @@ def drawPieces(screen, board):
             piece = board[row][col]
             if piece != "--": #not empty square
                 screen.blit(images[piece], p.Rect(col*sq_size, row*sq_size, sq_size, sq_size))
-
-
 
 
 main()
